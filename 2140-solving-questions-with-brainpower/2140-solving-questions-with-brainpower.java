b8fa182fb13fd5e60ab0de29c9fd2700809e95cc
amir ammar <am7099533@gmail.com>
2022-05-13 12:44:14 +0200

    Time: 5 ms (93.75%), Space: 92.3 MB (97.79%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
@@ -1,18 +1,16 @@
 class Solution {
-    static long [] memo;
-
-    public static long dp(int idx, int n, int [][] arr) {
-        if (idx >= n) return 0;
-        if (memo[idx] != -1) return memo[idx];
-        return memo[idx] = Math.max(arr[idx][0] + dp(idx + 1 + arr[idx][1], n, arr), dp(idx + 1, n, arr));
-    }
-
-
     public static long mostPoints(int[][] questions) {
 
         int n = questions.length;
-        memo = new long[n];
-        Arrays.fill(memo, -1);
-        return dp(0, n, questions);
+        long [] memo = new long[n];
+        memo[n-1] = questions[n-1][0];
+        for (int i = n-2; i >= 0; i--) {
+            if (i + questions[i][1] + 1 < n) {
+                memo[i] = Math.max(memo[i+1], questions[i][0] + memo[i + questions[i][1] + 1]);
+            }else {
+                memo[i] = Math.max(memo[i+1], questions[i][0]);
+            }
+        }
+        return memo[0];
     }
 }
