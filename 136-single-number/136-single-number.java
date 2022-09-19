c6c33b2dd32310c694d81bc2c3c96def0345ef8c
amir ammar <amir.ammar@student.guc.edu.eg>
2022-09-19 16:09:36 +0200

    Time: 2 ms (70.49%), Space: 50.1 MB (78.49%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,9 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int x = 0;
-        for(int i = 0 ; i < nums.length; i++){
-            x ^= nums[i];
+        for(int i = 1 ; i < nums.length; i++){
+            nums[0] ^= nums[i];
         }
-        return x;
+        return nums[0];
     }
 }
